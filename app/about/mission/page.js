727e25b2d5164d21377bb8d4ba862c6159a4cf15
MD IBN ALAM <97562405+ibnalam@users.noreply.github.com>
2024-03-09 21:57:28 +0600

    image optimization

--- app/about/mission/page.js
+++ app/about/mission/page.js
@@ -1,15 +1,18 @@
 
 import Button from '@/app/components/Button'
+import Image from 'next/image'
+import fb from '@/public/images/fb.png'
 import React from 'react'
 
 const mission = () => {
   return (
    <main>
     <div>Mission Page </div>
-
+    <div className=''>
+    <Image src={fb} alt="fb image"/>
+    </div>
     <div className='mt-5'>
       <Button/>
-     
     </div>
    </main>
   )
